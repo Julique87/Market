bdd8d094986d2d3a3782de9944d3bf699375cab6
Julia Novikova <julikova.n@gmail.com>
2023-06-03 17:47:05 +0300

    add Repository and Manager

--- src/main/java/ru/netology/Manager.java
+++ src/main/java/ru/netology/Manager.java
@@ -1,2 +1,38 @@
-package ru.netology;public class Manager {
+package ru.netology;
+
+public class Manager {
+    private static Repository repo;
+
+    public Manager(Repository repo) {
+        this.repo = repo;
+    }
+
+    public void add(Product product) {               // Static?
+        repo.add(product);
+    }
+
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repo.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
--- src/main/java/ru/netology/Smartphone.java
+++ src/main/java/ru/netology/Smartphone.java
@@ -1,2 +1,14 @@
-package ru.netology;public class Smartphone {
+package ru.netology;
+
+public class Smartphone extends Product {
+    private String company;
+
+    public Smartphone(int id, String name, int price, String company) {
+        super(id, name, price);
+        this.company = company;
+    }
+
+    public String getCompany() {
+        return company;
+    }
 }
